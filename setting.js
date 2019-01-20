4b232a572fc8b08bad70bd8368d78f945b3c2823
Toshiyuki Masui <masui@pitecan.com>
2019-01-20 20:58:46 +0900

    README追加

--- setting.js
+++ setting.js
@@ -1,15 +1,5 @@
-//var abc = document.getElementById('abc')
-//alert(abc)
-//abc.addEventListener('click',function(){alert(localStorage['abcde'])})
-//localStorage['abcde'] = 'abcde'
-
 
 $(function(){
-    //var button = document.getElementById('submit')
-    //alert(button)
-    //button.addEventListener('click',register)
-
-
     chrome.storage.sync.get(["secret"], (item) => $('#secret').val(item.secret))
 
     $('#submit').on('click', ()=> {
